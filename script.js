6ac3362dc56748afd665501da9657dffcfcd0472
soffiprivv1-beep <soffi.privv1@gmail.com>
2026-06-19 14:10:31 -0600

    Crear archivo script.js con funcionalidad de cursos

---
+++ script.js
@@ -0,0 +1,23 @@
+function mostrar(){
+
+document
+.getElementById("cursos")
+.innerHTML=`
+
+<ul>
+
+<li>Comunicación</li>
+
+<li>Liderazgo</li>
+
+<li>Investigación</li>
+
+<li>Negocios</li>
+
+<li>Arte</li>
+
+</ul>
+
+`
+
+}
